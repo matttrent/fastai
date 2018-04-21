fccadb96e48b22fe458bab295254b674937d2a65
Matthew Trentacoste <web+git@matttrent.com>
2018-04-20 18:51:19 -0700

    setup for gans package

--- setup-gans.py
+++ setup-gans.py
@@ -18,24 +18,15 @@ from setuptools import setup
 ##########################################################################
 
 setup(
-    name = "fastai",
-    packages = ['fastai', 'fastai/models', 'fastai/models/cifar10'],
-    version = 0.6 ,
-    description = "The fast.ai deep learning and machine learning library. Git pull fastai, for all fast.ai sessions and tutorials also",
-    author = "Jeremy Howard and contributors",
-    author_email = "info@fast.ai",
+    name = "fastai_gans",
+    packages = ['gans', 'gans/data', 'gans/models', 'gans/options', 'gans/util'],
+    version = 0.1 ,
+    description = "CycleGAN and Pix2Pix",
+    author = "",
+    author_email = "",
     license = "Apache License 2.0",
-    url = "https://github.com/fastai/fastai",
-    download_url =  'https://github.com/fastai/fastai/archive/0.6.tar.gz',
     install_requires =
-     ['awscli', 'bcolz', 'bleach', 'certifi', 'cycler', 'decorator', 'entrypoints', 'feather-format', 'graphviz', 'html5lib',
-      'ipykernel', 'ipython', 'ipython-genutils', 'ipywidgets', 'isoweek', 'jedi', 'Jinja2', 'jsonschema', 'jupyter',
-      'jupyter-client', 'jupyter-console', 'jupyter_contrib_nbextensions', 'jupyter-core', 'kaggle-cli', 'MarkupSafe',
-      'matplotlib', 'mistune', 'nbconvert', 'nbformat', 'notebook', 'numpy', 'olefile', 'opencv-python', 'pandas',
-      'pandas_summary', 'pandocfilters', 'pexpect', 'pickleshare', 'Pillow', 'plotnine', 'prompt-toolkit',
-      'ptyprocess', 'Pygments', 'pyparsing', 'python-dateutil', 'pytz', 'PyYAML', 'pyzmq', 'qtconsole', 'scipy',
-      'seaborn', 'simplegeneric', 'six', 'sklearn_pandas', 'terminado', 'testpath', 'torchtext', 'tornado', 'tqdm',
-      'traitlets', 'wcwidth', 'webencodings', 'widgetsnbextension'],
+     ['visdom', 'dominator'],
     keywords = ['deeplearning', 'pytorch', 'machinelearning'],
     classifiers = ['Development Status :: 3 - Alpha', 'Programming Language :: Python', 'Programming Language :: Python :: 3.6']
 )
